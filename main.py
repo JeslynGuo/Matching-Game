6f44f4e3bf0200018578bd4c826bf926c7acd15c
JeslynGuo <jeslynguoisawesome@gmail.com>
2020-06-16 17:43:03 +0000

    Added when game statements. Completed matching game.

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def print_board():
   for r in range(rows):
     print('{} | '.format(r+1)+ ' '.join(characters[int(board[r,c,1])]        
     if board[r,c,0] == 1.0 else u'\u2588' for c in range(cols)))
-
 def get_choice(limit, pos, turn):
   while True:
     try: 
@@ -37,10 +36,43 @@ def get_choice(limit, pos, turn):
         break
   return id
 
-
 def main():
   make_board()
   print_board()
+  match_count = 0
+  attempts  = 0
+  first_flip = True
+  while match_count < len(characters):
+    if first_flip:
+      r1 = get_choice(rows, 'row', 'first')
+      c1 = get_choice(cols, 'column', 'first')
+      if board[r1, c1, 0] == 1.0:
+        print("That card is already flipped.")
+      else:
+        first_flip = False
+        board[r1, c1, 0] = 1.0
+        print_board()
+    # second flip
+    else: 
+       r2 = get_choice(rows, 'row', 'second')
+       c2 = get_choice(cols, 'column', 'second')
+       if board[r2, c2, 0] == 1.0:
+        print("That card is already flipped.")
+       else:
+        attempts += 1
+        first_flip = True
+        board[r2, c2, 0] = 1.0
+        print_board()
+        if board[r1, c1,1] == board[r2,c2,1]:
+          print('You got a match!')
+          match_count += 1
+        else:
+          print('No match! Better luck next time.')
+          board[r1, c1, 0] = 0.0
+          board[r2, c2, 0] = 0.0
+          print_board()
+  print("You completeed the game in {} attempts".format(attempts))
 
 if __name__ == '__main__':
   main()
+
